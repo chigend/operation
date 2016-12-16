e052070c68702e7780870af826f633bd19e42a55
yejinbiao <651523696@qq.com>
2016-12-16 13:48:45 +0800

    login

--- src/test/java/test/MainTest.java
+++ src/test/java/test/MainTest.java
@@ -1,9 +1,9 @@
 package test;
 
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
 
@@ -14,9 +14,8 @@ import java.net.InetAddress;
 
 public class MainTest {
    public static  void main(String[]args) throws Exception{
-      TransportClient client = new PreBuiltTransportClient(Settings.EMPTY)
-              .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("host1"), 9300))
-              .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("host2"), 9300));
+      TransportClient client = TransportClient.builder().build()
+              .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
 
 // on shutdown
 
