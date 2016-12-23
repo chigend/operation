d2e9e77364d11ac5300456d8aa3d34d884eae449
yjb <651523696@qq.com>
2016-12-23 13:53:51 +0800

    add ad

--- src/main/java/com/cookabuy/controller/AdController.java
+++ src/main/java/com/cookabuy/controller/AdController.java
@@ -1,16 +1,19 @@
 package com.cookabuy.controller;
 
 import com.cookabuy.constant.AdPageName;
+import com.cookabuy.entity.service.dto.AddAdForm;
 import com.cookabuy.entity.service.dto.DisPlayAd;
 import com.cookabuy.entity.service.po.Ad;
 import com.cookabuy.repository.service.AdRepository;
 import com.cookabuy.util.DozerHelper;
 import com.cookabuy.util.Result;
+import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -24,6 +27,8 @@ public class AdController {
     private AdRepository adRepository;
     @Autowired
     private DozerHelper dozerHelper;
+    @Autowired
+    private DozerBeanMapper dozerBeanMapper;
     @RequestMapping("ads")
     public Result findIndexAds(Result result){
         List<Ad> ads = adRepository.findByPageNameOrderByPositionAsc(AdPageName.INDEX);
@@ -31,6 +36,15 @@ public class AdController {
         List<DisPlayAd> disPlayAds = dozerHelper.mapList(ads,DisPlayAd.class);
         result.addData("ads",disPlayAds);
         return  result;
+    }
+
+    @RequestMapping("add_ad")
+    public Result addAd(AddAdForm form,Result result){
+        Ad ad = dozerBeanMapper.map(form,Ad.class);
+        ad.setCreateTime(new Date());
+        ad.setPageName(AdPageName.INDEX);
+        adRepository.save(ad);
+        return result;
     }
 
 }
---
+++ src/main/java/com/cookabuy/entity/service/dto/AddAdForm.java
@@ -0,0 +1,18 @@
+package com.cookabuy.entity.service.dto;
+
+import java.util.Date;
+
+/**
+ * @author yejinbiao
+ * @create 2016-12-23-13:30
+ */
+
+public class AddAdForm {
+
+    private String picUrl;
+
+    private String activityUrl;
+
+    private Integer position;
+
+}
