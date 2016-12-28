ccc98c0e0e48a99cf8f5982e0e8a6f874135a2af
yjb <651523696@qq.com>
2016-12-28 13:32:17 +0800

    transactional

--- src/main/java/com/cookabuy/repository/service/RecommendStoreRepository.java
+++ src/main/java/com/cookabuy/repository/service/RecommendStoreRepository.java
@@ -3,7 +3,9 @@ import com.cookabuy.entity.service.po.RecommendStore;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface RecommendStoreRepository extends JpaRepository<RecommendStore,Integer> {
     @Override
     <S extends RecommendStore> S save(S entity);
