5be68f4a339ef19b82ffe97e622864ae404f7086
yjb <651523696@qq.com>
2016-12-28 14:17:57 +0800

    delete recommend store

--- src/main/java/com/cookabuy/controller/RecommendController.java
+++ src/main/java/com/cookabuy/controller/RecommendController.java
@@ -1,11 +1,13 @@
 package com.cookabuy.controller;
 
+import com.cookabuy.constant.CosConstant;
 import com.cookabuy.constant.PageContant;
 import com.cookabuy.entity.service.dto.RecommendStoreDTO;
 import com.cookabuy.entity.service.po.RecommendStore;
 import com.cookabuy.entity.service.po.Store;
 import com.cookabuy.repository.service.RecommendStoreRepository;
 import com.cookabuy.repository.service.StoreRepository;
+import com.cookabuy.thirdParty.cos.FileHelper;
 import com.cookabuy.thirdParty.dozer.DozerHelper;
 import com.cookabuy.validator.CompoundValidator;
 import com.cookabuy.validator.ReplaceRecommendFormValidator;
@@ -25,8 +27,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.io.File;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 2016/12/5
@@ -41,6 +45,8 @@ public class RecommendController {
     private StoreRepository storeRepository;
     @Autowired
     private DozerHelper dozerHelper;
+    @Autowired
+    private FileHelper fileHelper;
     @RequestMapping("/recommend_store")
     public Result recommendStore(@RequestBody List<RecommendStoreDTO> stores){
         List<RecommendStore> recommendStores = dozerHelper.mapList(stores, RecommendStore.class);
@@ -68,6 +74,17 @@ public class RecommendController {
             dto.setStoreName(store.getStoreName());
         });
         result.addData("stores", dtos);
+        return result;
+    }
+
+    @RequestMapping("delete_store")
+    public Result deleteStore(Integer id, Result result){
+        Optional<RecommendStore> recommendStore = Optional.ofNullable(recommendStoreRepository.findOne(id));
+        //如果推荐店铺的url存在表示该图片已经上传cos，那么在删除该店铺之前首先删除在cos上的图片
+        recommendStore.map(RecommendStore::getPicUrl).ifPresent(url->{
+            fileHelper.deleteFile(CosConstant.DIRECOTRY_PREFIX_STORE_PATH,url);
+        });
+        recommendStore.map(RecommendStore::getId).ifPresent(recommendStoreRepository::delete);
         return result;
     }
 
